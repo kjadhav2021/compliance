4acb349c705783331ef9a3e3ec1bae0a40a94351
Kunal Jadhav <kjadhav102015@gmail.com>
2021-05-25 17:03:36 +0800

    commented classes

--- manifests/init.pp
+++ manifests/init.pp
@@ -7,8 +7,8 @@
 class compliance {
   include compliance::windows::item::w_2
   include compliance::windows::item::w_13
-  include compliance::windows::item::w_14
-  include compliance::windows::item::w_17
-  include compliance::windows::item::w_18
-  include compliance::windows::item::w_19
+  # include compliance::windows::item::w_14
+  # include compliance::windows::item::w_17
+  # include compliance::windows::item::w_18
+  # include compliance::windows::item::w_19
 }
