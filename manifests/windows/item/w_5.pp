76aa4eeca32843f2601b141f781165613d4b7b8b
Kunal Jadhav <kjadhav102015@gmail.com>
2021-05-28 14:36:41 +0800

    Added hiera configurations

--- manifests/windows/item/w_5.pp
+++ manifests/windows/item/w_5.pp
@@ -6,14 +6,23 @@
 #   include compliance::windows::item::w_5
 class compliance::windows::item::w_5 {
   # $registry_titles = [ $::screensaverissecuretitle, $::screensavertimeouttitle]
-  $registry_keys = [ $::screensaverissecurekey,$::screensavertimeoutkey]
-  $registry_values = [$::screensaverissecurevalue,$::screensavertimeoutvalue]
+  # $registry_keys = [ $::screensaverissecurekey,$::screensavertimeoutkey]
+  # $registry_values = [$::screensaverissecurevalue,$::screensavertimeoutvalue]
 
   if $facts['cis_local_sids'] {
     $facts['cis_local_sids'].each |$sid| {
-      # notify{ $sid:}
-      notify{ $registry_keys: }
-      # notify{ $registry_values: }
+      registry::value { $::screensaverissecuretitle :
+        key   => $::screensaverissecurekey,
+        value => $::screensaverissecurevalue,
+        type  => string,
+        data  => $::screensaverissecuredata,
+      }
+      registry::value { $::screensavertimeouttitle :
+        key   => $::screensavertimeoutkey,
+        value => $::screensavertimeoutvalue,
+        type  => string,
+        data  => $::screensavertimeoutdata,
+      }
     }
   }
 }
