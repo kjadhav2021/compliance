bce4f33f9dba60fe2f9a842878b38a3c9898807d
Kunal Jadhav <kjadhav102015@gmail.com>
2021-05-28 14:57:13 +0800

    reverted changes

--- manifests/windows/item/w_5.pp
+++ manifests/windows/item/w_5.pp
@@ -7,13 +7,13 @@
 class compliance::windows::item::w_5 {
   if $facts['cis_local_sids'] {
     $facts['cis_local_sids'].each |$sid| {
-      registry::value { "HKEY_USERS\\${sid}\\Software\\Policies\\Microsoft\\Windows\\Control Panel\\Desktop:ScreenSaverIsSecure": # lint:ignore:140chars
+      registry::value { $::screensaverissecurevalue :
         key   => "HKEY_USERS\\${sid}\\Software\\Policies\\Microsoft\\Windows\\Control Panel\\Desktop",
         value => $::screensaverissecurevalue,
         type  => string,
         data  => $::screensaverissecuredata,
       }
-      registry::value { "HKEY_USERS\\${sid}\\Software\\Policies\\Microsoft\\Windows\\Control Panel\\Desktop:ScreenSaveTimeOut": # lint:ignore:140chars
+      registry::value { $::screensavertimeoutvalue :
         key   => "HKEY_USERS\\${sid}\\Software\\Policies\\Microsoft\\Windows\\Control Panel\\Desktop",
         value => $::screensavertimeoutvalue,
         type  => string,
