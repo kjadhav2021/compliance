074c7a342acd39c5a640d7ffba85225b6369f8e8
Kunal Jadhav <kjadhav102015@gmail.com>
2021-05-28 15:04:52 +0800

    Added regedit entry

--- manifests/windows/item/w_5.pp
+++ manifests/windows/item/w_5.pp
@@ -7,15 +7,15 @@
 class compliance::windows::item::w_5 {
   if $facts['cis_local_sids'] {
     $facts['cis_local_sids'].each |$sid| {
-      registry::value { 'screensaverissecurevalue' :
+      registry::value { "HKEY_USERS\\${sid}\\Software\\Policies\\Microsoft\\Windows\\Control Panel\\Desktop:ScreenSaverIsSecure": # lint:ignore:140chars
         key   => "HKEY_USERS\\${sid}\\Software\\Policies\\Microsoft\\Windows\\Control Panel\\Desktop",
-        value => $::screensaverissecurevalue,
+        value => 'ScreenSaverIsSecure',
         type  => string,
         data  => $::screensaverissecuredata,
       }
-      registry::value { 'screensavertimeoutvalue' :
+      registry::value { "HKEY_USERS\\${sid}\\Software\\Policies\\Microsoft\\Windows\\Control Panel\\Desktop:ScreenSaveTimeOut": # lint:ignore:140chars
         key   => "HKEY_USERS\\${sid}\\Software\\Policies\\Microsoft\\Windows\\Control Panel\\Desktop",
-        value => $::screensavertimeoutvalue,
+        value => 'ScreenSaveTimeOut',
         type  => string,
         data  => $::screensavertimeoutdata,
       }
