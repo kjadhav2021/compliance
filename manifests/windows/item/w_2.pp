1fd3a3e85feb9e567e422695585ca7c39f02a131
Kunal Jadhav <kjadhav102015@gmail.com>
2021-06-30 02:09:40 +0800

    modified w_2

--- manifests/windows/item/w_2.pp
+++ manifests/windows/item/w_2.pp
@@ -19,7 +19,7 @@
 # @param system_timezone default system timezone.
 class compliance::windows::item::w_2 (
   Boolean $report_only    = true,
-  String  $system_timezone = 'Malay Peninsula Standard Time',
+  String  $system_timezone = 'Singapore Standard Time',
 ){
   # The below line sets this class and any contained classes/resources to noop/reporting mode
   if $report_only { noop() }
@@ -37,16 +37,15 @@ class compliance::windows::item::w_2 (
   # Below this line comes all Puppet code required to enforce the standard
   # ----------------------------------------------------------------------
   if $facts['timezone'] {
-    if $facts['timezone'] != 'Malay Peninsula Standard Time' {
+    if $facts['timezone'] != 'Singapore Standard Time' {
       if $report_only {
         notify { compliance::policy_title($item_id, $item_title, $setting_desc, 'the timezone has not configured correctly.'):
         message => 'Non-Compliant',
       }
       }
     else {
-      local_security_policy { 'Change the time zone':
-        ensure       => present,
-        policy_value => $system_timezone,
+      class { 'timezone_win':
+      timezone => $system_timezone,
       }
     }
   }
@@ -57,3 +56,8 @@ class compliance::windows::item::w_2 (
     }
   }
 }
+
+# local_security_policy { 'Change the time zone':
+      #   ensure       => present,
+      #   policy_value => $system_timezone,
+      # }
