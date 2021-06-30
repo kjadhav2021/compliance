35fbfa491e1ea793aacae1799e28e5074551f452
Kunal Jadhav <kjadhav102015@gmail.com>
2021-06-30 12:00:12 +0800

    Added missing facts

--- manifests/windows/item/w_5.pp
+++ manifests/windows/item/w_5.pp
@@ -42,18 +42,16 @@ class compliance::windows::item::w_5 (
   if $facts['cis_local_sids'] {
     $facts['cis_local_sids'].each |$sid| {
       registry::value { "HKEY_USERS\\${sid}\\Software\\Policies\\Microsoft\\Windows\\Control Panel\\Desktop:ScreenSaverIsSecure": # lint:ignore:140chars
-        ensure => present,
-        key    => "HKEY_USERS\\${sid}\\Software\\Policies\\Microsoft\\Windows\\Control Panel\\Desktop",
-        value  => 'ScreenSaverIsSecure',
-        type   => string,
-        data   => $screensaverissecuredata,
+        key   => "HKEY_USERS\\${sid}\\Software\\Policies\\Microsoft\\Windows\\Control Panel\\Desktop",
+        value => 'ScreenSaverIsSecure',
+        type  => string,
+        data  => $screensaverissecuredata,
       }
       registry::value { "HKEY_USERS\\${sid}\\Software\\Policies\\Microsoft\\Windows\\Control Panel\\Desktop:ScreenSaveTimeOut": # lint:ignore:140chars
-        ensure => present,
-        key    => "HKEY_USERS\\${sid}\\Software\\Policies\\Microsoft\\Windows\\Control Panel\\Desktop",
-        value  => 'ScreenSaveTimeOut',
-        type   => string,
-        data   => $screensavertimeoutdata,
+        key   => "HKEY_USERS\\${sid}\\Software\\Policies\\Microsoft\\Windows\\Control Panel\\Desktop",
+        value => 'ScreenSaveTimeOut',
+        type  => string,
+        data  => $screensavertimeoutdata,
       }
     }
   }
