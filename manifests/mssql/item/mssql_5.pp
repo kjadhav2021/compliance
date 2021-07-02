2ba7f33ef1c55d0a5b58c561725ff72db9f9fe29
Kunal Jadhav <kjadhav102015@gmail.com>
2021-07-02 11:55:15 +0800

    mssql_5 : added report_only boolean parameter

--- manifests/mssql/item/mssql_5.pp
+++ manifests/mssql/item/mssql_5.pp
@@ -20,7 +20,25 @@
 #                          REVOKE CONNECT FROM GUEST
 #
 # @param report_only Whether or not to set the resources to noop mode
-class compliance::mssql::item::mssql_5 {
+class compliance::mssql::item::mssql_5 (
+  Boolean $report_only = true,
+){
+  # The below line sets this class and any contained classes/resources to noop/reporting mode
+  if $report_only { noop() }
+
+  Notify {
+    tag       => ['compliance_rule'],
+    loglevel  => 'debug',
+  }
+
+  $item_id      = 'mssql_5'
+  $item_title   = 'Remove Guest User ID from the database'
+  $setting_desc = 'If the user ID guest exists in the database, all logins not mapped or aliased to a user ID are allowed
+  access to the database as guest. The guest account cannot be removed from the master, msdb, model, tempdb and distribution databases.
+  The use of the guest user ID in other databases should be limited.' # lint:ignore:140chars
+
+  # Below this line comes all Puppet code required to enforce the standard
+  # ----------------------------------------------------------------------
   # Resource to connect to the DB instance
   sqlserver::config { 'SQLEXPRESS':
     admin_login_type => 'WINDOWS_LOGIN'
